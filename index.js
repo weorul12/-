7b07e363b1dbabe05ef96641dfe0e1bdff6a979e
Your Name <you@example.com>
2021-10-11 08:37:16 +0000

    pract89

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs').promises;
+
+class Todos {
+    constructor() {
+        this.todos = [];
+    }
+
+    list() {
+        return [...this.todos];
+    }
+
+    add(title) {
+        let todo = {
+            title: title,
+            completed: false,
+        }
+
+        this.todos.push(todo);
+    }
+
+    complete(title) {
+	 if (this.todos.length === 0) {
+          throw new Error("You have no TODOs stored. Why don't you add one first?");
+    }
+        let todoFound = false;
+        this.todos.forEach((todo) => {
+            if (todo.title === title) {
+                todo.completed = true;
+                todoFound = true;
+                return;
+            }
+        });
+
+        if (!todoFound) {
+            throw new Error(`No TODO was found with the title: "${title}"`);
+    }
+ }
+
+ saveToFile() {
+        let fileContents = 'Title,Completed\n';
+        this.todos.forEach((todo) => {
+            fileContents += `${todo.title},${todo.completed}\n`
+        });
+
+        return fs.writeFile('todos.csv', fileContents);
+ }
+}
+module.exports = Todos;
---
+++ index.test.js
@@ -0,0 +1,75 @@
+const Todos = require('./index');
+const assert = require('assert').strict;
+const fs = require('fs');
+
+describe("integration test", function() {
+    it("should be able to add and complete TODOs", function() {
+        let todos = new Todos();
+        assert.strictEqual(todos.list().length, 0);
+
+        todos.add("run code");
+        assert.strictEqual(todos.list().length, 1);
+        assert.deepStrictEqual(todos.list(), [{title: "run code", completed: false}]);
+
+        todos.add("test everything");
+        assert.strictEqual(todos.list().length, 2);
+        assert.deepStrictEqual(todos.list(),
+            [
+                { title: "run code", completed: false },
+                { title: "test everything", completed: false }
+            ]
+        );
+
+        todos.complete("run code");
+        assert.deepStrictEqual(todos.list(),
+            [
+                { title: "run code", completed: true },
+                { title: "test everything", completed: false }
+            ]
+    );
+  });
+});
+
+
+
+describe("complete()", function() {
+    it("should fail if there are no TODOs", function() {
+        let todos = new Todos();
+        const expectedError = new Error("You have no TODOs stored. Why don't you add one first?");
+
+        assert.throws(() => {
+            todos.complete("doesn't exist");
+        }, expectedError);
+    });
+});
+describe("saveToFile()", function () {
+    beforeEach(function () {
+        this.todos = new Todos();
+        this.todos.add("save a CSV");
+    });
+
+    afterEach(function () {
+        if (fs.existsSync("todos.csv")) {
+            fs.unlinkSync("todos.csv");
+        }
+    });
+
+    it("should save a single TODO without error", async function () {
+        await this.todos.saveToFile();
+
+        assert.strictEqual(fs.existsSync("todos.csv"), true);
+        let expectedFileContents = "Title,Completed\nsave a CSV,false\n";
+        let content = fs.readFileSync("todos.csv").toString();
+        assert.strictEqual(content, expectedFileContents);
+    });
+
+    it("should save a single TODO that's completed", async function () {
+        this.todos.complete("save a CSV");
+        await this.todos.saveToFile();
+
+        assert.strictEqual(fs.existsSync('todos.csv'), true);
+        let expectedFileContents = "Title,Completed\nsave a CSV,true\n";
+        let content = fs.readFileSync("todos.csv").toString();
+        assert.strictEqual(content, expectedFileContents);
+    });
+});
